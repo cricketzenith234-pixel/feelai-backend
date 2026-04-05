f11abf77e2ab48ab1b4964a236ca09d17b14780b
cricketzenith234-pixel <cricketzenith234@gmail.com>
2026-04-05 21:43:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/chat", async (req, res) => {
     const userMessage = req.query.message;
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + process.env.GEMINI_API_KEY,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + process.env.GEMINI_API_KEY,
       {
         method: "POST",
         headers: {
